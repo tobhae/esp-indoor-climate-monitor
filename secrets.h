f05d102483ec2d91d7b407a06c46f4fadf61ee14
tobhae <tobhae.dev@pm.me>
2026-02-22 00:26:34 +0100

    Header for esp32_climate_node, read instructions

---
+++ secrets.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/*
+This file is read by the node.
+Enter your credentials and make sure it is added to .gitignore.
+*/
+
+// WiFi
+#define WIFI_SSID "your_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+// InfluxDB
+#define INFLUX_HOST "ipv4_address"
+#define INFLUX_ORG "influxdb_org"
+#define INFLUX_BUCKET "influxdb_bucket"
+#define INFLUX_TOKEN "your_influxdb_token"
