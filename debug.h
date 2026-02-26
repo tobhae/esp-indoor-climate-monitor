138e914586e9669983909922107f8ac284f5b275
tobhae <tobhae.dev@pm.me>
2026-02-26 20:56:09 +0100

    Added a description in debug.h

--- debug.h
+++ debug.h
@@ -1,6 +1,12 @@
 #pragma once
 
-/* TODO: Some information about debug.h */
+/* Provides compile-time debug macros for Serial output.
+
+   DEBUG = 1, debug statements enabled.
+   DEBUG = 0, debug statements disabled. 
+   
+   When disabled, all debug statements are removed during preprocessing. 
+   It is recommended to disable DEBUG when deploying a node. */
 
 #define DEBUG 1
 
